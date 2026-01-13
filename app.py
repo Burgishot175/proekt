c44f19bf4f9f32c52dd4bfab3010c94c1f2a30ec
Burgishot175 <deyan.petkov22@gmail.com>
2026-01-13 12:23:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,30 +4,42 @@ import pandas as pd
 st.title(" Любими неща класна анкета")
 
 # Инициализация на данните
+if "subjects" not in st.session_state:
+    st.session_state.subjects = {
+        "Математика": 0,
+        "Български": 0,
+        "Програмиране": 0,
+        "Физическо": 0
+        "Физика": 0
+    }
 if "colors" not in st.session_state:
     st.session_state.colors = {
-        "Червен": 0,
-        "Син": 0,
-        "Зелен": 0,
-        "Жълт": 0
-    }
+        "red": 0,
+        "orange": 0,
+        "yellow": 0,
+        "green": 0,
+      "blue": 0
+      }
   
-if "sports" not in st.session_state:
-    st.session_state.sports = {
-        "Футбол": 0,
-        "Баскетбол": 0,
-        "Волейбол": 0,
-      "Плуване": 0
+if "grades" not in st.session_state:
+    st.session_state.grades = {
+        "6": 0,
+        "5": 0,
+        "4": 0,
+        "3": 0,
+      "2": 0
       }
   
 st.subheader("Избери любими неща")
 
-color = st.selectbox("Любим цвят:", list(st.session_state.colors.keys()))
-sport = st.selectbox("Любим спорт:", list(st.session_state.sports.keys()))
+subject = st.selectbox("Любим предмет:", list(st.session_state.subjects.keys()))
+grade = st.selectbox("Оценки:", list(st.session_state.grades.keys()))
+color = st.selectbox("Оценки:", list(st.session_state.colorss.keys()))
 
 if st.button("Запази избора"):
-    st.session_state.colors [color] += 1
-    st.session_state.sports [sport] += 1
+    st.session_state.subjects [color] += 1
+    st.session_state.grades [sport] += 1
+    st.session_state.colors [sport] += 1
     st.success("Изборът е записан!")
   
 st.divider()
@@ -37,14 +49,20 @@ st.subheader(" Резултати")
 # Графика за цветовете
 st.write("Любими цветове")
 colors_df = pd.DataFrame.from_dict(
-    st.session_state.colors, orient="index", columns=["Брой"]
+    st.session_state.subjects, orient="index", columns=["Брой"]
 )
 
-st.bar_chart(colors_df)
+st.bar_chart(subjects_df)
 
 # Графика за спортовете
 st.write("Любими спортове")
 sports_df = pd. DataFrame.from_dict(
-    st.session_state.sports, orient="index", columns=["Брой"]
+    st.session_state.grades, orient="index", columns=["Брой"]
 )
-st.bar_chart(sports_df)
+st.bar_chart(grades_df)
+
+st.write("Любими спортове")
+sports_df = pd. DataFrame.from_dict(
+    st.session_state.colors, orient="index", columns=["Брой"]
+)
+st.bar_chart(colors_df)
